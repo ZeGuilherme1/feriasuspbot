e239c78ec2cb0f4af463015b21667e90ed8c149d
José Guilherme Santos Riberto <145349759+ZeGuilherme1@users.noreply.github.com>
2024-09-02 16:40:25 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import express from 'express';
 import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
@@ -6,43 +5,43 @@ import * as process from 'process';
 
 dotenv.config();
 
-let diasRestantes = 102 ;       
 const agent = new BskyAgent({
   service: 'https://bsky.social',
 });
+
+function getDiasRestantes(): number {
+  const hoje = new Date();
+  const dataAlvo = new Date('2024-12-12');
+  const diffTime = dataAlvo.getTime() - hoje.getTime();
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
+}
 
 async function diasCounter() {  
   try {
     await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD! });
 
+    const diasRestantes = getDiasRestantes();
+
     if (diasRestantes > 0) {
-      diasRestantes--; 
       await agent.post({
-        text: `Faltam ${diasRestantes} dias para as férias da USP!`  
+        text: `Faltam ${diasRestantes} dias para as férias da USP!`
       });
       console.log(`Postado: Faltam ${diasRestantes} dias para as férias da USP!`);
     } else {
       console.log("As férias começaram?!");
     }
   } catch (error) {
-    console.error("Erro ao tentar postar a contagem de dias:", error, "número de dias: ", diasRestantes);
+    console.error("Erro ao tentar postar a contagem de dias:", error);
   }
 }
 
-const scheduleExpressionDaily = '0 0 * * *'; 
+const scheduleExpressionDaily = '0 0 * * *';
 
 const job = new CronJob(scheduleExpressionDaily, diasCounter);
 diasCounter();
 console.log("BOT iniciado. Postando a cada 24 horas...");
+console.log('Username:', process.env.BLUESKY_USERNAME);
+console.log('Password:', process.env.BLUESKY_PASSWORD);
 job.start();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Bot está funcionando!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor está escutando na porta ${PORT}`);
-});
